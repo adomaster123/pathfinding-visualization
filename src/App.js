3e2abfa936f650270ef453fd58ea37b0cc3ef4e3
[Adogbeji Ibori] <aibori@sjs.org>
2021-04-07 14:40:01 -0500

    bugs included

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  // Declaring the state variable that will store the layout of the 20x20 maze
+  let arr = [[]];
+  let arr1 = new Array(20);
+    arr1.fill(0);
+  for (let i = 0; i < 20; i++) {
+    arr[i] = arr1;
+  }
+  const [grid, setGrid] = useState(arr);
+  const [mode, setMode] = useState("obstacle");
+
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    let rowID = "row" + i;
+    let cell =[];
+    for (let idx = 0; idx < grid.length; idx++) {
+      let cellID = "cell" + i + "-" + idx;
+      cell.push(<td onClick={() => editHandler(i, idx)} style={{backgroundColor: grid[i][idx] == 0 ? "gray" : grid[i][idx] == 's' ? "green" : grid[i][idx] == "g" ? "red" : "black", padding: "min(16px, 1.5vw)"}} className="cell" key={cellID} id={cellID}></td>)
+    }
+
+    rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+  }
+
+  function editHandler(y, x) {
+    switch(mode) {
+      case "obstacle":
+        let copy = grid.slice();
+        copy[y][x] = 1;
+        setGrid(copy);
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pathfinding Algorithm Visualization Tool</h1>
+      <div className="button-container">
+        <button className="buttons">Start</button>
+        <button className="buttons">Goal</button>
+        <button className="buttons" onClick={() => setMode("obstacles")}>Obstacles</button>
+        <button className="buttons">Randomize</button>
+        <button className="buttons">Delete</button>
+        <button className="buttons">Clear</button>
+      </div>
+      <div className="grid-container">
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+      <div className="button-container">
+        <select className="buttons">
+          <option>Breadth First Search</option>
+          <option>Depth First Search</option>
+          <option>A*</option>
+        </select>
+        <button className="buttons"> Find Path</button>
+      </div>
     </div>
   );
 }
